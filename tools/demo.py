990c1cee8b52ba36c354de4a82c5e1f7f8a433f5
hdd <hdd1073394498@gmail.com>
2021-12-07 21:20:05 +0800

    sb train test demo can run

--- tools/demo.py
+++ tools/demo.py
@@ -164,7 +164,7 @@ def detect(cfg,opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='weights/End-to-end.pth', help='model.pth path(s)')
+    parser.add_argument('--weights', nargs='+', type=str, default='/workspace/Disk/hdd/YOLOP/runs/BddDataset/checkpoint.pth', help='model.pth path(s)')
     parser.add_argument('--source', type=str, default='inference/videos', help='source')  # file/folder   ex:inference/images
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
