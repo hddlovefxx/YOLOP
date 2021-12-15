d38c9350172b7ddf1cffb3d374c96ffc49c9779c
hdd <hdd1073394498@gmail.com>
2021-12-15 15:28:54 +0800

    demo.py can run

--- tools/demo.py
+++ tools/demo.py
@@ -52,7 +52,7 @@ def detect(cfg,opt):
     # Load model
     model = get_net(cfg)
     checkpoint = torch.load(opt.weights, map_location= device)
-    model.load_state_dict(checkpoint['state_dict'])
+    model.load_state_dict(checkpoint)
     model = model.to(device)
     if half:
         model.half()  # to FP16
@@ -164,8 +164,8 @@ def detect(cfg,opt):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='/workspace/Disk/hdd/YOLOP/runs/BddDataset/checkpoint.pth', help='model.pth path(s)')
-    parser.add_argument('--source', type=str, default='inference/videos', help='source')  # file/folder   ex:inference/images
+    parser.add_argument('--weights', nargs='+', type=str, default='/workspace/Disk/hdd/YOLOP/runs/BddDataset/_2021-12-14-22-08/final_state.pth', help='model.pth path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder   ex:inference/images
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
